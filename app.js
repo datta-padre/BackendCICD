4035f811585f04e5b9984c81ebfb0f10dcfb2cac
Datta Padre <datta.padre@gmail.com>
2026-02-10 13:37:51 +0530

    deploy

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 app.use(express.json()); 
 
 app.get("/", (req, res) => {
-    res.send("Welcome TO React Node MongoDB Application");
+    res.send("Welcome TO React Node MongoDB Application Hiii");
 });
 
 app.use("/user", UserRoute);
